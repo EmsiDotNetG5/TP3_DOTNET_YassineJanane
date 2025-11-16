15a49b9b42a60aa8e714471dc69a438c9f9d8203
idk-2000 <saadjanane0@gmail.com>
2025-11-16 21:25:32 +0100

    tp3_dotnet

---
+++ gestion_memoire/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+class Logger : IDisposable
+{
+    private StreamWriter writer;
+
+    public Logger(string path)
+    {
+        writer = new StreamWriter(path, true);
+    }
+
+    public void Ecrire(string message)
+    {
+        writer.WriteLine(DateTime.Now + " : " + message);
+    }
+
+    public void Dispose()
+    {
+        writer?.Close();
+        Console.WriteLine("fichier journal fermé");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        using (StreamWriter sw = new StreamWriter("etudiants.txt"))
+        {
+            sw.WriteLine("Bonjour EMSI !");
+            sw.WriteLine("Ce fichier est cree avec StreamWriter");
+        }
+        Console.WriteLine("Fichier créé et fermé automatiquement");
+
+        using (StreamReader sr = new StreamReader("etudiants.txt"))
+        {
+            Console.WriteLine("Contenu du fichier :");
+            Console.WriteLine(sr.ReadToEnd());
+        }
+
+        using (StreamReader sr = new StreamReader("etudiants.txt"))
+        using (StreamWriter sw = new StreamWriter("copie.txt"))
+        {
+            string ligne;
+            while ((ligne = sr.ReadLine()) != null)
+            {
+                sw.WriteLine(ligne);
+            }
+        }
+        Console.WriteLine("Copie terminée !");
+
+        using (var log = new Logger("journal.txt"))
+        {
+            log.Ecrire("démarrage du programme");
+            log.Ecrire("fin du programme");
+        }
+    }
+}
